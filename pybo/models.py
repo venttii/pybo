e7438c74055d786c2187af15e208afc00d88a791
ventti <ehdgus2357@gmail.com>
2021-01-14 14:29:09 +0900

    admin에서 오트젝트 내용 보이게 수정

--- pybo/models.py
+++ pybo/models.py
@@ -21,6 +21,9 @@ class Answer(models.Model):
     modify_date = models.DateTimeField(null=True, blank=True)
     voter = models.ManyToManyField(User, related_name='voter_answer')
 
+    def __str__(self):
+        return self.content
+
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
@@ -28,3 +31,6 @@ class Comment(models.Model):
     modify_date = models.DateTimeField(null=True, blank=True)
     question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
